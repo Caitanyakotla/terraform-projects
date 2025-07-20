57999dafdde5a93c07958eaee00e7c9bf34e61b3
sai krishna chaitanya kotla <sai.krishna.chaitanya.kotla@ericsson.com>
2025-07-21 00:13:35 +0200

    Terraform files of Ec2

--- main.tf
+++ main.tf
@@ -14,4 +14,8 @@ resource "aws_instance" "public_instance" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
+
+    tags = {
+    Name = "public_instance-${count.index}"   #public_instance-0, public_instance-1, public_instance-2 
+  }
 }
